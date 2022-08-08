9b101e5307a728a319f912377f720d3d2371fa13
jas3333 <accounts1@protonmail.com>
2022-08-08 16:45:32 -0700

    Reverse string function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let new_string = '';
+    let string_length = string.length - 1;
 
+    for (string_length; string_length >= 0; string_length--) {
+        new_string += string[string_length];
+    }
+
+    return new_string;
 };
 
 // Do not edit below this line
