cb9a2d35621afd7fc5e1a7bb19192517552a5aee
jas3333 <accounts1@protonmail.com>
2022-08-08 16:40:02 -0700

    Created repeat string function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, repeats) {
 
+    let updated_string = '';
+
+    if (repeats < 0)
+        return 'ERROR';
+
+    for (let i = 0; i < repeats; i++) {
+        updated_string += string;
+    }
+
+    return updated_string;
 };
 
 // Do not edit below this line
